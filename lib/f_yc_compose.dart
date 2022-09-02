4ef1aa242eda6d71075e52a450c7ee80ef18dd5a
yhfwork <mm20102010mm>
2022-09-02 10:49:57 +0800

    init

--- lib/f_yc_compose.dart
+++ lib/f_yc_compose.dart
@@ -3,3 +3,7 @@ library f_yc_compose;
 export 'src/baidu/mtj/yc_baidu_mtj.dart';
 export 'src/att/yc_app_tracking_transparency.dart';
 export 'src/launch_review/yc_launch_review.dart';
+export 'src/connectivity/yc_connectivity.dart';
+export 'src/share/yc_share.dart';
+export 'src/version_upgrade/yc_version_upgrade.dart';
+export 'src/wechat/yc_wechat.dart';
