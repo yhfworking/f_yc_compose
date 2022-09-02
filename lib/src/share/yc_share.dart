a80d08257d2b5ce381a05845fb5a4bf14d65f9bd
yhfwork <mm20102010mm>
2022-09-02 10:53:17 +0800

    init

--- lib/src/share/yc_share.dart
+++ lib/src/share/yc_share.dart
@@ -1,8 +1,8 @@
 import 'package:f_yc_config/f_yc_config.dart';
 import 'package:share_plus/share_plus.dart';
 
-class ComponentsShare {
-  static void sharess() async {
+class YcShare {
+  static void sharesWithSystem() async {
     await Share.share(YcConfig.shareTitle());
   }
 }
